c060dc2229abbaecff3b546aa89be867e55e88a5
vlad <vlad.legusha@gmail.com>
2020-07-21 13:49:55 +0300

    added navigation links for Header.vue

---
+++ components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <section id="header" class="container-fluid p-0">
+    <div class="container">
+      <div>
+        <nuxt-link
+          v-for="(item, i) in navList"
+          :key="i"
+          :to="item.link"
+          class="nav-link d-inline"
+        >
+          {{item.title.toUpperCase()}}
+        </nuxt-link>
+      </div>
+      <div></div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "Header",
+    props: {
+      navList: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
