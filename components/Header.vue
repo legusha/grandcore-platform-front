d4326a9a30dece12e1225b59fd4cbdec3560dac4
vlad <vlad.legusha@gmail.com>
2020-07-22 14:42:58 +0300

    fix mobile items

--- components/Header.vue
+++ components/Header.vue
@@ -1,32 +1,37 @@
 <template>
   <section id="header" class="container-fluid p-0">
-    <div class="container d-flex justify-between align-center">
-      <div class="header-nav-link">
-        <nuxt-link
-          v-for="(navItem, i) in navList"
-          :key="i"
-          :to="navItem.link"
-          class="nav-link d-inline"
-        >
-          {{navItem.title.toUpperCase()}}
-        </nuxt-link>
+    <div class="container">
+      <div class="header-desktop d-flex justify-between align-center">
+        <div class="header-nav-link">
+          <nuxt-link
+            v-for="(navItem, i) in navList"
+            :key="i"
+            :to="navItem.link"
+            class="nav-link d-inline"
+          >
+            {{navItem.title.toUpperCase()}}
+          </nuxt-link>
+        </div>
+        <div class="header-action">
+          <button
+            type="button"
+            class="btn btn-outline-light"
+            @click="$emit('action')"
+          >
+            Войти</button>
+        </div>
       </div>
-      <div class="header-logo">
-        <img src="@images/logo-light.svg" alt="Logo">
-      </div>
-      <div class="header-action">
-        <button
-          type="button"
-          class="btn btn-outline-light"
-          @click="$emit('action')"
-        >
-          Войти</button>
-        <img
-          class="header-action-user"
-          src="@icons/user.svg"
-          alt="Avatar"
-          @click="$emit('action')"
-        >
+      <div class="header-mobile d-flex justify-between align-center">
+        <div class="header-logo">
+          <img src="@images/logo-light.svg" alt="Logo">
+        </div>
+        <div class="header-action-user">
+          <img
+            src="@icons/user.svg"
+            alt="Avatar"
+            @click="$emit('action')"
+          >
+        </div>
       </div>
     </div>
   </section>
