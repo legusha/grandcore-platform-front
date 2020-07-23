57aae3bc45f74461cebd0f984ecff43b27657e38
vlad <vlad.legusha@gmail.com>
2020-07-23 11:50:09 +0300

    all layouts part moved from pages to default.vue layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,68 @@
 <template>
-  <nuxt/>
+  <div class="pos-relative">
+    <Header
+      class="pos-relative layer-1"
+      :navList="navList"
+      :activeBurger="activeBurger"
+      @action="actionHeader"
+      @actionBurger="actionBurger"
+    ></Header>
+    <Sidebar
+      :class="{'layer-2': activeBurger}"
+      :navList="navList"
+      :active="activeBurger"
+      @toggle="toggleSidebar"
+    >
+    </Sidebar>
+    <nuxt class="pos-relative layer-1"></nuxt>
+    <Footer class="layer-1"></Footer>
+  </div>
 </template>
+<script>
+  import Header from "~/components/Header";
+  import Sidebar from "~/components/Sidebar";
+  import Footer from "~/components/Footer";
+
+  export default {
+    name: "default",
+    components: {
+      Sidebar,
+      Header,
+      Footer
+    },
+    data () {
+      return {
+        navList: [
+          {
+            title: 'О ФОНДЕ',
+            link: ''
+          },
+          {
+            title: 'ПОЖЕРТВОВАНИЯ',
+            link: ''
+          },
+          {
+            title: 'УЧАСТНИКИ',
+            link: ''
+          },
+          {
+            title: 'НОВОСТИ',
+            link: ''
+          },
+        ],
+        activeBurger: false
+      }
+    },
+    methods: {
+      actionHeader () {
+      },
+      actionBurger () {
+        this.activeBurger = !this.activeBurger
+      },
+      toggleSidebar () {
+        this.activeBurger = !this.activeBurger
+      }
+    }
+  }
+</script>
 
